cead253bd021dae5f70be52d7b8bc9fcf1fc78fc
zenitovich <zenitovich@gmail.com>
2023-05-04 18:04:17 +0300

    started func

--- public/main.ts
+++ public/main.ts
@@ -14,3 +14,12 @@ slider.init()
 
 slider.init()
 
+function riceValue (min: number, max: number, value: number) {
+    let num = max - min
+    let interval = num / (value + 1)
+    for (let i = 0; i < value; i+1) {
+        return min + (interval * i)
+    }
+}
+
+console.log(riceValue(2, 10, 3))
