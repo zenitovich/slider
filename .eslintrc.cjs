98d4d2c5d15c62c711de2dc4b4d56a11e0ca7bf0
zenitovich <deniskaupenas@MacBook-Air-Denis.local>
2023-05-29 21:14:02 +0300

    added airbnb-base

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,9 +4,10 @@ module.exports = {
         "es2021": true
     },
     "extends": [
+        "prettier",
         "eslint:recommended",
+        "airbnb-base",
         "plugin:@typescript-eslint/recommended",
-        "prettier"
     ],
     "overrides": [
     ],
