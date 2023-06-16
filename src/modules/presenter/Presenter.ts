279db6c607913ee0aca5ca63b3f175ec8d9fbf3d
zenitovich <zenitovich@gmail.com>
2023-06-16 15:25:10 +0300

    printWidth in prettier changed

--- src/modules/presenter/Presenter.ts
+++ src/modules/presenter/Presenter.ts
@@ -9,13 +9,10 @@ export default class Presenter {
 
   public method(pointCoords: DOMRect, eventPageX: number) {
     const pointLength: number = pointCoords.right - pointCoords.x;
-    const pointRange: number =
-      this.model.scaleData.max - this.model.scaleData.min;
+    const pointRange: number = this.model.scaleData.max - this.model.scaleData.min;
     const pointStep: number = pointRange / pointLength;
     if (eventPageX >= pointCoords.x && eventPageX <= pointCoords.right) {
-      const pointValue: number =
-        Math.round((eventPageX - pointCoords.x) * pointStep) +
-        this.model.scaleData.min;
+      const pointValue: number = Math.round((eventPageX - pointCoords.x) * pointStep) + this.model.scaleData.min;
       this.model.pointData = {
         valueElemHtml: `${pointValue}`,
         pointButtonPosition: `${eventPageX - pointCoords.x - 11}px`,
